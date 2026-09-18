3ecb3d3ca0803a28dad29eab4e8ce98d14c19493
sarwoadjikaton <sarwoadjikaton@gmail.com>
2026-09-18 15:50:33 +0700

    Update footer badge pair logo SiTrack and Kemnaker with clear styling

--- public/build/assets/Index-Sl6DaILB.js
+++ public/build/assets/Index-Sl6DaILB.js
@@ -1,4 +1,4 @@
-import{d as vl,K as Mc,z as Sc,E as yc,o as Ct,f as Nt,g as ts,b as ae,h as Ec,i as bc,v as Tc,e as Ft,a as Rs,w as Dr,u as Ir,m as Ml,L as Ac,s as Sl,c as co,Z as wc,n as Rc,t as Et,j as mn,p as Cc,F as Pc,r as Lc,x as uo,N as Dc,Q as Ic}from"./app-Dz1uzOcT.js";import{P as Uc}from"./PublicLayout--01LHirE.js";import{_ as Nc}from"./StatusBadge.vue_vue_type_script_setup_true_lang-CyB1EWfW.js";import{_ as yl}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./ToastNotification.vue_vue_type_script_setup_true_lang-BA6X0BDN.js";/**
+import{d as vl,K as Mc,z as Sc,E as yc,o as Ct,f as Nt,g as ts,b as ae,h as Ec,i as bc,v as Tc,e as Ft,a as Rs,w as Dr,u as Ir,m as Ml,L as Ac,s as Sl,c as co,Z as wc,n as Rc,t as Et,j as mn,p as Cc,F as Pc,r as Lc,x as uo,N as Dc,Q as Ic}from"./app-Cu704-q2.js";import{P as Uc}from"./PublicLayout-BQqytP6a.js";import{_ as Nc}from"./StatusBadge.vue_vue_type_script_setup_true_lang-9IekJiCt.js";import{_ as yl}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./ToastNotification.vue_vue_type_script_setup_true_lang-CVSNNh7c.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
